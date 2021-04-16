9c5518f774e99ee18030b42b62b730b078571774
AndyLinxies <linxies@gmail.com>
2021-04-16 16:35:28 +0200

    Exo 3 array

--- exo3-Array/exo4-Array/public/js/main.js
+++ exo3-Array/exo4-Array/public/js/main.js
@@ -2,7 +2,7 @@ let maVar = ['Coding 11','Coding 12','Coding 13'];
 console.log(maVar);
 maVar.push('Andy');
 console.log(maVar);
-maVar.splice(0,0,'Coding18')
+maVar.splice(1,1,'Coding18') //Cod12 a l'index 1 est supprimé et on met Cod18 a la place
 console.log(maVar);
 maVar.splice(1,1);
 console.log(maVar);
--- exo3-Array/public/js/main.js
+++ exo3-Array/public/js/main.js
@@ -2,4 +2,4 @@ let maVar = ['Coding 11','Coding 12','Coding 13'];
 console.log(maVar);
 maVar.push('Andy');
 console.log(maVar);
-maVar.splice(0,0,'Coding18')
+maVar.splice(0,1,'Coding18') //Remplace le premier element (Index0, par Coding18)
